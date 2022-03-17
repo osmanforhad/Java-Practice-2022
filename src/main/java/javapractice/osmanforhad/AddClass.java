c66ff50ae40ce26c6e6136c3195684f4f912775c
Osman Forhad <osmanforhad93@gmail.com>
2022-03-17 23:30:58 +0600

    working with function paramiter concept

---
+++ src/main/java/javapractice/osmanforhad/AddClass.java
@@ -0,0 +1,20 @@
+package javapractice.osmanforhad;
+
+/**
+ *
+ * @author osman forhad
+ */
+public class AddClass {
+    
+    public void add(int a, int b){
+        int c = a+b;
+        System.out.println(c);
+    }
+    
+    public void multiply(int a, int b){
+        int c = a*b;
+        System.out.println(c);
+    }
+    
+    
+}
--- src/main/java/javapractice/osmanforhad/Javapractice.java
+++ src/main/java/javapractice/osmanforhad/Javapractice.java
@@ -7,8 +7,13 @@ package javapractice.osmanforhad;
 public class Javapractice {
 
     public static void main(String[] args) {
+        //Working With OOP JAVA Concept
         MyClass ObjectVariable = new MyClass();
         ObjectVariable.myfunction();
         ObjectVariable.myfunction2();
+        //Working With Function Parameter Concept
+        AddClass ObjectHolder = new AddClass();
+        ObjectHolder.add(6, 5);
+        ObjectHolder.multiply(6, 5);
     }
 }
