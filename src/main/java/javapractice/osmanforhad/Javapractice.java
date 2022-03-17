55c9f8fe796621a91cbde2fd4d603cffa76f9908
Osman Forhad <osmanforhad93@gmail.com>
2022-03-17 22:40:47 +0600

    working with oop concept in java programing

--- src/main/java/javapractice/osmanforhad/Javapractice.java
+++ src/main/java/javapractice/osmanforhad/Javapractice.java
@@ -7,6 +7,8 @@ package javapractice.osmanforhad;
 public class Javapractice {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MyClass ObjectVariable = new MyClass();
+        ObjectVariable.myfunction();
+        ObjectVariable.myfunction2();
     }
 }
---
+++ src/main/java/javapractice/osmanforhad/MyClass.java
@@ -0,0 +1,15 @@
+package javapractice.osmanforhad;
+
+/**
+ *
+ * @author osman forhad
+ */
+public class MyClass {
+    
+    public void myfunction(){
+        System.out.println("My First OOP JAVA");
+    }
+    public void myfunction2(){
+        System.out.println("My Second OOP JAVA");
+    }
+}
