a8eca809895c7d56d7f2f2f6fb93496e8f08f22e
Osman Forhad <osmanforhad93@gmail.com>
2022-03-18 23:59:59 +0600

    working with super keyword concept in constructor method

--- src/main/java/javapractice/osmanforhad/SubClass.java
+++ src/main/java/javapractice/osmanforhad/SubClass.java
@@ -6,6 +6,13 @@ package javapractice.osmanforhad;
  */
 public class SubClass extends SuperClass{
    
+    //__Constructor Method__//
+    SubClass(){
+        super();//__calling the constructor method from super class__//
+        System.out.println("Constructor From Sub Class");
+    }
+    
+    
     //__Override__//
     void Print(){
         super.Print();//__Calling Method from Super Class__//
--- src/main/java/javapractice/osmanforhad/SuperClass.java
+++ src/main/java/javapractice/osmanforhad/SuperClass.java
@@ -5,6 +5,11 @@ package javapractice.osmanforhad;
  * @author osman forhad
  */
 public class SuperClass {
+    
+    //__Constructor Method__//
+      SuperClass(){
+        System.out.println("Constructor From Super Class");
+    }
      
      void Print(){
         System.out.println("From Super Class");
