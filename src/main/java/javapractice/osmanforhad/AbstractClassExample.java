eb786c53436cd7b8d34aaf9ebb185cb3d7121967
Osman Forhad <osmanforhad93@gmail.com>
2022-03-19 11:20:57 +0600

    working with abstract method concept in java

--- src/main/java/javapractice/osmanforhad/AbstractClassExample.java
+++ src/main/java/javapractice/osmanforhad/AbstractClassExample.java
@@ -5,12 +5,15 @@ package javapractice.osmanforhad;
  * @author osman forhad
  */
 abstract class AbstractClassExample {
-    void demo(){
-        System.out.println("This is The Abstract Class");
-    }
+    
+    //this is the abstract method
+    abstract void demo();
 }
 
 //__Helper Class to make accessable the Abastract class by making an object in main class__//
 class Helper extends AbstractClassExample{
-
+    @Override
+    void demo(){
+        System.out.println("This is Abstract Method which override from abstract class");
+    }
 }
